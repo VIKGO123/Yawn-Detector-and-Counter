d9aa6e8bdcb3118fb3f9524754ec447a218f13ca
VIKASH PATHAK <vikashpathak1121@gmail.com>
2018-10-30 23:26:45 +0530

    Updated yawn detector and counter with mouth tracker

--- yawn_detector_counter.py
+++ yawn_detector_counter.py
@@ -1,21 +1,19 @@
 import cv2
 import numpy as np
 import dlib
-path="/shape_predictor_68_face_landmarks.dat"
+path="C:/Users/hp/Desktop/AI/Udemy CV/shape_predictor_68_face_landmarks.dat"
 predictor=dlib.shape_predictor(path)
 detector=dlib.get_frontal_face_detector()
 
 
-class Toomanyfaces(Exception):
-    pass
-class Toofewfaces(Exception):
-    pass
 def get_landmarks(im):
     rects=detector(im,1)#image and no.of rectangles to be drawn
     if len(rects)>1:
-        raise Toomanyfaces
+        print("Toomanyfaces")
+        return np.matrix([0])
     if len(rects)==0:
-        raise Toofewfaces
+        print("Toofewfaces")
+        return np.matrix([0])
     return np.matrix([[p.x,p.y] for p in predictor(im,rects[0]).parts()])   
 def place_landmarks(im,landmarks):
     im=im.copy()
@@ -32,7 +30,6 @@ def upper_lip(landmarks):
         top_lip.append(landmarks[j])
     top_lip_point=(np.squeeze(np.asarray(top_lip)))
     top_mean=np.mean(top_lip_point,axis=0)
-    print(top_mean)
     return int(top_mean[1])
     
         
@@ -44,29 +41,50 @@ def low_lip(landmarks):
         lower_lip.append(landmarks[j])
     lower_lip_point=(np.squeeze(np.asarray(lower_lip)))
     lower_mean=np.mean(lower_lip_point,axis=0)
+    
+        
     return int(lower_mean[1])
- #getting mean coordinates of lower and upper lips and getting vertical distance between them             
+               
 def decision(image):
     landmarks=get_landmarks(image)
+    if(landmarks.all()==[0]):
+        return -10#Dummy value to prevent error
     top_lip=upper_lip(landmarks)
     lower_lip=low_lip(landmarks)
     distance=abs(top_lip-lower_lip)
     return distance
     
+    
+
+    
+    
+        
+              
 cap=cv2.VideoCapture(0)
 yawns=0
 while(True):
     ret,frame=cap.read()
-    #landmarks=get_landmarks(frame)
-    
-    #cv2.imshow("Total Landmarks",place_landmarks(frame,landmarks))
-    distance=decision(frame)
-    if(distance>20):
-        yawns=yawns+1
-    cv2.putText(frame,"Yawn Count: "+str(yawns),(50,100),fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,fontScale=1.0,color=(0,255,255))
-    cv2.imshow("Subject Yawn Count",frame)
-    if cv2.waitKey(1)==13:
-        break
+    if(ret==True):
+        
+        landmarks=get_landmarks(frame)
+        if(landmarks.all()!=[0]):
+            l1=[]
+            for k in range(48,60):
+                l1.append(landmarks[k])
+            l2=np.asarray(l1)
+            lips=cv2.convexHull(l2)
+            cv2.drawContours(frame, [lips], -1, (0, 255, 0), 1)
+        
+        distance=decision(frame)
+        if(distance>21):    #Use distance according to your convenience
+            yawns=yawns+1
+            
+        cv2.putText(frame,"Yawn Count: "+str(yawns),(50,100),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1.0,color=(0,255,255))
+        cv2.imshow("Subject Yawn Count",frame)
+        if cv2.waitKey(1)==13:
+            break
+    else:
+        continue
 cap.release()
 cv2.destroyAllWindows()    
     
